dad9799aed2d4c0daead6a6d2a33e2ed54ef6602
Iced Quinn <icedquinn@iceworks.cc>
2021-12-12 02:02:06 -0600

    feat: THeader protocol

--- src/thriftcore/theader.nim
+++ src/thriftcore/theader.nim
@@ -1,183 +1,148 @@
-import macros, varint, codecutil
-
-const
-   ZlibTransform*   = 0x01
-   HmacTransform*   = 0x02
-   SnappyTransform* = 0x03
-
-   ProtocolIdBinary*  = 0x00
-   ProtocolIdCompact* = 0x02
-
-   InfoKeyValue* = 0x01
-
-   HeaderMagic* = 0x0fff
+import protocol, theaderimpl
 
 type
-   THeaderHeader* = object
-      length*: uint32 ## Size of the payload being wrapped.
-      flags*: uint16
-      sequence_number*: uint32
-      protocol_id*: int
-      key_values*: seq[(string, string)]
-      transforms: seq[int] ## Technically transforms can accept data blocks but no supported transform does. Keep this variable access controlled for future upgrades.
+   THeaderProtocol* = ref object of Protocol
+      header*: THeaderHeader ## Holds header for this particular reading.
+      buffer*: string        ## Writes bytes to this buffer.
+      wrapped*: Protocol     ## Other write calls are deferred to this.
+      here*: int             ## Holds current position while reading.
 
-macro rnok(x: untyped): untyped =
-   ## Call X then test if 'ok' is true otherwise return.
-   ## Common idiom when testing if parsers were successful.
-   result = quote:
-      `x`
-      if not ok: return
+const
+   ENotImplemented = "Method not implemented."
 
-proc read_theader*(source: string; here: var int; ok: var bool): THeaderHeader =
-   let valid = 0..source.high
-   ok = false
-   let mark = here
-   defer:
-      if not ok:
-         here = mark
+#/-
+#| Write messages
+#\-
 
-   var magic, headersize: uint16
-
-   rnok: result.length = read_u32be(source, here, ok)
-   let length_tracking_starts = here
-   rnok: magic = read_u16be(source, here, ok)
-   rnok: result.flags = read_u16be(source, here, ok)
-   rnok: result.sequence_number = read_u32be(source, here, ok)
-   rnok: headersize = read_u16be(source, here, ok)
-   let header_size_begins = here
-   rnok: result.protocol_id = read_varint(source, here, ok).int
-
-   let transform_count = read_varint(source, here, ok).int
-   if not ok: return
-   for i in 0..<transform_count:
-      let transform = read_varint(source, here, ok)
-      if not ok: return
-      case transform
-      of ZlibTransform, HmacTransform, SnappyTransform:
-         result.transforms.add transform.int
-      else:
-         # XXX no way to feedback this is unsupported transform
-         return
-
-   # now we read header pairs until we run out of data or hit an unknown
-   let needle = header_size_begins + (headersize.int * 4)
-   while here < needle:
-      let info_id = read_varint(source, here, ok)
-      if not ok: return
-      case info_id:
-      of InfoKeyValue:
-         let count = read_varint(source, here, ok)
-         if not ok: return
-         for i in 0..<count:
-            if here notin valid: return
-            let klen = read_varint(source, here, ok)
-            if not ok: return
-            if here+klen notin valid: return
-            let key = source.substr(here, here+(klen.int-1))
-            inc here, klen.int
-
-            if here notin valid: return
-            let vlen = read_varint(source, here, ok)
-            if not ok: return
-            if here+vlen notin valid: return
-            let value = source.substr(here, here+(vlen.int-1))
-            inc here, vlen.int
-
-            result.key_values.add((key, value))
-
-      else:
-         # we're allowed to skip remaining infos if we don't recognize one
-         here = needle
-
-   # make sure to skip past the padding too
-   here = needle
-
-   # correct length for payload only
-   let header_stops_here = here
-   result.length -= (header_stops_here - length_tracking_starts).uint32
-
-   ok = true
-
-proc write_theader*(source: var string; value: THeaderHeader; ok: var bool) =
-   ok = false
-
-   # TODO endian nonsense; we assume host is little and we're posting to network byte order
-   let full_length_pos = source.len
-   pad(source, 4)
-   let start_size_check_pos = source.len
-
-   put_u16be(source, HeaderMagic) # add header magic
-   put_u16be(source, value.flags) # add flags
-   put_u32be(source, value.sequence_number) # add sequence number
-
-   # add remainin header bytes / 4
-   let header_remainder_pos = source.len
-   pad(source, 2)
-   let header_remainder_tracking_pos = source.len
-
-   # add protocol ID
-   rnok: write_varint(source, value.protocol_id, ok)
-
-   # add transform count and transforms
-   rnok: write_varint(source, value.transforms.len, ok)
-   for v in value.transforms:
-      rnok: write_varint(source, v, ok)
-
-   # write INFO segment and its key/value pairs
-   rnok: write_varint(source, InfoKeyValue, ok)
-   rnok: write_varint(source, value.keyvalues.len, ok)
-   for kv in value.keyvalues:
-      rnok: write_varint(source, kv[0].len, ok)
-      source.add kv[0]
-
-      rnok: write_varint(source, kv[1].len, ok)
-      source.add kv[1]
-
-   let end_headers_pos_a = source.len
-
-   # make sure header size is padded to word boundary
-   let hsize = end_headers_pos_a - header_remainder_tracking_pos
-   let hmod = (hsize mod 4)
-   if hmod > 0:
-      for i in hmod..<4: source.add 0.char
-
-   let end_headers_pos_b = source.len
-
-   let payload_size = (end_headers_pos_b - start_size_check_pos).uint32 + value.length
-   set_u32be(source, payload_size, full_length_pos)
-
-   let sanity = (end_headers_pos_b - header_remainder_tracking_pos) mod 4
-   assert sanity == 0
-
-   let header_size = (end_headers_pos_b - header_remainder_tracking_pos) div 4
-   set_u16be(source, header_size.uint16, header_remainder_pos)
-
-   ok = true
-
-when is_main_module:
-   var h: THeaderHeader
-   h.length = 32
-   h.protocol_id = ProtocolIdCompact
-   h.sequence_number = 1337
-   h.keyvalues.add ("spudger", "bongled")
-   h.keyvalues.add ("jwt", "shittified")
-
-   echo h
-   var buf: string
-   var here: int
+method write_message_begin*(self: THeaderProtocol; name: string; kind: MessageKind; sequence_number: int32) =
    var ok: bool
-   write_theader(buf, h, ok)
-   assert ok == true
-   buf.add "xxx"
+   write_theader(self.buffer, self.header, ok)
+   if not ok:
+      raise new_exception(Exception, "Writing failed.")
+   write_message_begin(self.wrapped, name, kind, sequence_number)
 
-   var h2 = read_theader(buf, here, ok)
-   assert ok == true
-   echo h2
+method write_message_end*(self: THeaderProtocol) =
+   write_message_end(self.wrapped) 
 
-   assert h.length == h2.length
-   assert h.flags == h2.flags
-   assert h.sequence_number == h2.sequence_number
-   assert h.protocol_id == h2.protocol_id
-   assert h.key_values == h2.key_values
-   assert h.transforms == h2.transforms
+method write_struct_begin*(self: THeaderProtocol; name: string) =
+   write_struct_begin(self.wrapped, name) 
+
+method write_struct_end*(self: THeaderProtocol) =
+   write_struct_end(self.wrapped)
+
+method write_field_begin*(self: THeaderProtocol; name: string; kind: TypeKind; id: int) =
+   write_field_begin(self.wrapped, name, kind, id) 
+
+method write_field_end*(self: THeaderProtocol) =
+   write_field_end(self.wrapped)
+
+method write_field_stop*(self: THeaderProtocol) =
+   write_field_stop(self.wrapped) 
+
+method write_map_begin*(self: THeaderProtocol; key_kind,value_kind: TypeKind; size: int) =
+   write_map_begin(self.wrapped, key_kind, value_kind, size) 
+
+method write_map_end*(self: THeaderProtocol) =
+   write_map_end(self.wrapped)
+
+method write_list_begin*(self: THeaderProtocol; value_kind: TypeKind; size: int) =
+   write_list_begin(self.wrapped, value_kind, size) 
+
+method write_list_end*(self: THeaderProtocol) =
+   write_list_end(self.wrapped)
+
+method write_set_begin*(self: THeaderProtocol; element_kind: TypeKind; size: int) =
+   write_set_begin(self.wrapped, element_kind, size) 
+
+method write_set_end*(self: THeaderProtocol) =
+   write_set_end(self.wrapped) 
+
+method write_bool*(self: THeaderProtocol; value: bool) =
+   write_bool(self.wrapped, value) 
+
+method write_byte*(self: THeaderProtocol; value: byte) =
+   write_byte(self.wrapped, value) 
+
+method write_i16*(self: THeaderProtocol; value: int16) =
+   write_i16(self.wrapped, value) 
+
+method write_i32*(self: THeaderProtocol; value: int32) =
+   write_i32(self.wrapped, value) 
+
+method write_i64*(self: THeaderProtocol; value: int64) =
+   write_i64(self.wrapped, value) 
+
+method write_double*(self: THeaderProtocol; value: float64) =
+   write_double(self.wrapped, value) 
+
+method write_string*(self: THeaderProtocol; value: string) =
+   write_string(self.wrapped, value) 
+
+#/-
+#| Read messages
+#\-
+
+method read_message_begin*(self: THeaderProtocol; name: var string; kind: var MessageKind; sequence_number: var int32) =
+   var ok: bool
+   self.header = read_theader(self.buffer, self.here, ok)
+   if not ok:
+      raise new_exception(Exception, "Reading failed.")
+   read_message_begin(self.wrapped, name, kind, sequence_number)
+
+method read_message_end*(self: THeaderProtocol) =
+   read_message_end(self.wrapped)
+
+method read_struct_begin*(self: THeaderProtocol; name: var string) =
+   read_struct_begin(self.wrapped, name)
+
+method read_struct_end*(self: THeaderProtocol) =
+   read_struct_end(self.wrapped)
+
+method read_field_begin*(self: THeaderProtocol; name: var string; kind: var TypeKind; id: var int) =
+   read_field_begin(self.wrapped, name, kind, id)
+
+method read_field_end*(self: THeaderProtocol) =
+   read_field_end(self.wrapped)
+
+method read_map_begin*(self: THeaderProtocol; key_kind, value_kind: var TypeKind; size: var int) =
+   read_map_begin(self.wrapped, key_kind, value_kind, size)
+
+method read_map_end*(self: THeaderProtocol) =
+   read_map_end(self.wrapped)
+
+method read_list_begin*(self: THeaderProtocol; value_kind: var TypeKind; size: var int) =
+   read_list_begin(self.wrapped, value_kind, size)
+
+method read_list_end*(self: THeaderProtocol) =
+   read_list_end(self.wrapped)
+
+method read_set_begin*(self: THeaderProtocol; element_kind: var TypeKind; size: var int) =
+   read_set_begin(self.wrapped, element_kind, size)
+
+method read_set_end*(self: THeaderProtocol) =
+   read_set_end(self.wrapped)
+
+method read_field_stop*(self: THeaderProtocol) =
+   read_field_stop(self.wrapped)
+
+method read_bool*(self: THeaderProtocol; value: var bool) =
+   read_bool(self.wrapped, value)
+
+method read_byte*(self: THeaderProtocol; value: var byte) =
+   read_byte(self.wrapped, value)
+
+method read_i16*(self: THeaderProtocol; value: var int16) =
+   read_i16(self.wrapped, value)
+
+method read_i32*(self: THeaderProtocol; value: var int32) =
+   read_i32(self.wrapped, value)
+
+method read_i64*(self: THeaderProtocol; value: var int64) =
+   read_i64(self.wrapped, value)
+
+method read_double*(self: THeaderProtocol; value: var float64) =
+   read_double(self.wrapped, value)
+
+method read_string*(self: THeaderProtocol; value: var string) =
+   read_string(self.wrapped, value)
 
---
+++ src/thriftcore/theaderimpl.nim
@@ -0,0 +1,183 @@
+import macros, varint, codecutil
+
+const
+   ZlibTransform*   = 0x01
+   HmacTransform*   = 0x02
+   SnappyTransform* = 0x03
+
+   ProtocolIdBinary*  = 0x00
+   ProtocolIdCompact* = 0x02
+
+   InfoKeyValue* = 0x01
+
+   HeaderMagic* = 0x0fff
+
+type
+   THeaderHeader* = object
+      length*: uint32 ## Size of the payload being wrapped.
+      flags*: uint16
+      sequence_number*: uint32
+      protocol_id*: int
+      key_values*: seq[(string, string)]
+      transforms: seq[int] ## Technically transforms can accept data blocks but no supported transform does. Keep this variable access controlled for future upgrades.
+
+macro rnok(x: untyped): untyped =
+   ## Call X then test if 'ok' is true otherwise return.
+   ## Common idiom when testing if parsers were successful.
+   result = quote:
+      `x`
+      if not ok: return
+
+proc read_theader*(source: string; here: var int; ok: var bool): THeaderHeader =
+   let valid = 0..source.high
+   ok = false
+   let mark = here
+   defer:
+      if not ok:
+         here = mark
+
+   var magic, headersize: uint16
+
+   rnok: result.length = read_u32be(source, here, ok)
+   let length_tracking_starts = here
+   rnok: magic = read_u16be(source, here, ok)
+   rnok: result.flags = read_u16be(source, here, ok)
+   rnok: result.sequence_number = read_u32be(source, here, ok)
+   rnok: headersize = read_u16be(source, here, ok)
+   let header_size_begins = here
+   rnok: result.protocol_id = read_varint(source, here, ok).int
+
+   let transform_count = read_varint(source, here, ok).int
+   if not ok: return
+   for i in 0..<transform_count:
+      let transform = read_varint(source, here, ok)
+      if not ok: return
+      case transform
+      of ZlibTransform, HmacTransform, SnappyTransform:
+         result.transforms.add transform.int
+      else:
+         # XXX no way to feedback this is unsupported transform
+         return
+
+   # now we read header pairs until we run out of data or hit an unknown
+   let needle = header_size_begins + (headersize.int * 4)
+   while here < needle:
+      let info_id = read_varint(source, here, ok)
+      if not ok: return
+      case info_id:
+      of InfoKeyValue:
+         let count = read_varint(source, here, ok)
+         if not ok: return
+         for i in 0..<count:
+            if here notin valid: return
+            let klen = read_varint(source, here, ok)
+            if not ok: return
+            if here+klen notin valid: return
+            let key = source.substr(here, here+(klen.int-1))
+            inc here, klen.int
+
+            if here notin valid: return
+            let vlen = read_varint(source, here, ok)
+            if not ok: return
+            if here+vlen notin valid: return
+            let value = source.substr(here, here+(vlen.int-1))
+            inc here, vlen.int
+
+            result.key_values.add((key, value))
+
+      else:
+         # we're allowed to skip remaining infos if we don't recognize one
+         here = needle
+
+   # make sure to skip past the padding too
+   here = needle
+
+   # correct length for payload only
+   let header_stops_here = here
+   result.length -= (header_stops_here - length_tracking_starts).uint32
+
+   ok = true
+
+proc write_theader*(source: var string; value: THeaderHeader; ok: var bool) =
+   ok = false
+
+   # TODO endian nonsense; we assume host is little and we're posting to network byte order
+   let full_length_pos = source.len
+   pad(source, 4)
+   let start_size_check_pos = source.len
+
+   put_u16be(source, HeaderMagic) # add header magic
+   put_u16be(source, value.flags) # add flags
+   put_u32be(source, value.sequence_number) # add sequence number
+
+   # add remainin header bytes / 4
+   let header_remainder_pos = source.len
+   pad(source, 2)
+   let header_remainder_tracking_pos = source.len
+
+   # add protocol ID
+   rnok: write_varint(source, value.protocol_id, ok)
+
+   # add transform count and transforms
+   rnok: write_varint(source, value.transforms.len, ok)
+   for v in value.transforms:
+      rnok: write_varint(source, v, ok)
+
+   # write INFO segment and its key/value pairs
+   rnok: write_varint(source, InfoKeyValue, ok)
+   rnok: write_varint(source, value.keyvalues.len, ok)
+   for kv in value.keyvalues:
+      rnok: write_varint(source, kv[0].len, ok)
+      source.add kv[0]
+
+      rnok: write_varint(source, kv[1].len, ok)
+      source.add kv[1]
+
+   let end_headers_pos_a = source.len
+
+   # make sure header size is padded to word boundary
+   let hsize = end_headers_pos_a - header_remainder_tracking_pos
+   let hmod = (hsize mod 4)
+   if hmod > 0:
+      for i in hmod..<4: source.add 0.char
+
+   let end_headers_pos_b = source.len
+
+   let payload_size = (end_headers_pos_b - start_size_check_pos).uint32 + value.length
+   set_u32be(source, payload_size, full_length_pos)
+
+   let sanity = (end_headers_pos_b - header_remainder_tracking_pos) mod 4
+   assert sanity == 0
+
+   let header_size = (end_headers_pos_b - header_remainder_tracking_pos) div 4
+   set_u16be(source, header_size.uint16, header_remainder_pos)
+
+   ok = true
+
+when is_main_module:
+   var h: THeaderHeader
+   h.length = 32
+   h.protocol_id = ProtocolIdCompact
+   h.sequence_number = 1337
+   h.keyvalues.add ("spudger", "bongled")
+   h.keyvalues.add ("jwt", "shittified")
+
+   echo h
+   var buf: string
+   var here: int
+   var ok: bool
+   write_theader(buf, h, ok)
+   assert ok == true
+   buf.add "xxx"
+
+   var h2 = read_theader(buf, here, ok)
+   assert ok == true
+   echo h2
+
+   assert h.length == h2.length
+   assert h.flags == h2.flags
+   assert h.sequence_number == h2.sequence_number
+   assert h.protocol_id == h2.protocol_id
+   assert h.key_values == h2.key_values
+   assert h.transforms == h2.transforms
+
